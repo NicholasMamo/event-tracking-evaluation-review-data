de898f28da7def040611bda1c396fd819e506cdc
Nicholas Mamo <nicholasmamo@gmail.com>
2022-09-06 15:42:11 +0200

    Add the library files used by the download tool

---
+++ lib/objects/attributable.py
@@ -0,0 +1,69 @@
+"""
+Attributable objects are normal objects that can have attributes.
+"""
+
+class Attributable(object):
+    """
+    Attributable objects have a dictionary that stores attributes.
+
+    :ivar attributes: The object's attributes.
+                      The keys are the attribute names and the values are the respective values.
+    :vartype attributes: dict
+    """
+
+    def __init__(self, attributes=None):
+        """
+        Initialize the object with the attributes.
+
+        :param attributes: The starting attributes.
+                           If ``None`` is given, an empty dictionary is created instead.
+        :type attributes: dict or None
+        """
+
+        self.attributes = attributes
+
+    def __getattr__(self, name):
+        """
+        The magic function through which most of the :class:`~Attributable`'s functionality passes.
+        This function receives any unknown call and tries to return an attribute with the same name.
+
+        .. note::
+
+            This function cannot be used to set an attribute.
+
+        :param name: The name of the attribute.
+        :type name: str
+
+        :return: The attribute value or `None` if it is not set.
+        :rtype: any or None
+        """
+
+        # fixes deepcopy recursing infinitely; attributes starting with two underscores can still be accessed but directly from the ``attributes`` dict 
+        if name.startswith('__'):
+            raise AttributeError(name)
+
+        return self.attributes.get(name)
+
+    @property
+    def attributes(self):
+        """
+        Get the attributes of this profile.
+
+        :return: The attributes stored in this profile.
+                 The attribute name is the key, and the value is the corresponding attribute value.
+        :rtype: dict
+        """
+
+        return self.__attributes
+
+    @attributes.setter
+    def attributes(self, attributes):
+        """
+        Override the attributes in this profile.
+
+        :param attributes: The new attributes.
+                           If ``None`` is given, an empty dictionary is initialized instead.
+        :type attributes: None or dict
+        """
+
+        self.__attributes = attributes or { }
---
+++ lib/objects/tests/test_attributable.py
@@ -0,0 +1,116 @@
+"""
+Run unit tests on the :class:`~objects.attributable.Attributable` class.
+"""
+
+import os
+import sys
+import unittest
+
+path = os.path.join(os.path.dirname(__file__), '..', '..')
+if path not in sys.path:
+    sys.path.append(path)
+
+from objects import Attributable
+from vsm import Vector
+
+class TestAttributable(unittest.TestCase):
+    """
+    Test the :class:`~objects.attributable.Attributable` class.
+    """
+
+    def test_create_empty(self):
+        """
+        Test that the empty :class:`~objects.attributable.Attributable` object has an empty dictionary.
+        """
+
+        self.assertEqual({ }, Attributable().attributes)
+
+    def test_create_None(self):
+        """
+        Test that a :class:`~objects.attributable.Attributable` object created with `None` attributes still has an empty dictionary.
+        """
+
+        self.assertEqual({ }, Attributable(None).attributes)
+
+    def test_create_with_data(self):
+        """
+        Test that an :class:`~objects.attributable.Attributable` object accepts attributes in the constructor.
+        """
+
+        self.assertEqual({ 'a': 1 }, Attributable({ 'a': 1 }).attributes)
+
+    def test_init_name_attributes_overwrite_none(self):
+        """
+        Test that overwriting the attributes with ``None`` creates an empty dictionary of attributes.
+        """
+
+        attributable = Attributable({ 'a': 1 })
+        self.assertEqual(1, attributable.a)
+
+        attributable.attributes = None
+        self.assertEqual({ }, attributable.attributes)
+
+    def test_get_attribute_as_property(self):
+        """
+        Test getting an attribute from the :class:`~objects.attributable.Attributable` as a property.
+        """
+
+        attributable = Attributable({ 'a': 1 })
+        self.assertEqual(1, attributable.a)
+
+    def test_get_non_existent_attribute_as_property(self):
+        """
+        Test getting an attribute that does not exist from the :class:`~objects.attributable.Attributable` as a property.
+        """
+
+        attributable = Attributable({ 'a': 1 })
+        self.assertEqual(None, attributable.b)
+
+    def test_set_attribute_from_property(self):
+        """
+        Test that attributes cannot be set using properties.
+        """
+
+        attributable = Attributable({ 'a': 1 })
+        attributable.b = 10
+        self.assertEqual(None, attributable.attributes.get('b'))
+
+    def test_get_attribute_underscores_attributeerror(self):
+        """
+        Test that getting an attribute that starts with two underscores raises an ``AttributeError``.
+        """
+
+        attributable = Attributable()
+        self.assertRaises(AttributeError, attributable.__getattr__, '__deepcopy__')
+
+    def test_get_attribute_underscores(self):
+        """
+        Test that getting an attribute that starts with two underscores can be accessed from the ``attributes`` dictionary.
+        """
+
+        attributable = Attributable({ '__deepcopy__': 'hello' })
+        self.assertEqual('hello', attributable.attributes['__deepcopy__'])
+
+    def test_get_attribute_as_property_from_derived_classes(self):
+        """
+        Test that attributes can be accessed as properties even in derived classes.
+        """
+
+        vector = Vector(dimensions={ 'a': 1 }, attributes={ 'b': 2 })
+        self.assertEqual({ 'b': 2 }, vector.attributes)
+
+    def test_get_actual_property_from_attributable(self):
+        """
+        Test that normal properties remain accessable.
+        """
+
+        vector = Vector(dimensions={ 'a': 1 })
+        self.assertEqual({ 'a': 1 }, vector.dimensions)
+
+    def test_call_function_not_property(self):
+        """
+        Test that normal functions remain accessable.
+        """
+
+        vector = Vector(dimensions={ 'a': 1 })
+        self.assertEqual(dict, type(vector.to_array()))
